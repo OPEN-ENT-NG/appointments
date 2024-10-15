03946bf6cdc905248c63629d4182ac8c7803eaf1
AMOSSE Nikolaï <niko@FR-GM7ZTL3.groupinfra.com>
2024-10-15 16:41:48 +0200

    feat(home): #RDV-23 theme + begin home page

---
+++ frontend/src/routes/home/style.ts
@@ -0,0 +1,7 @@
+import { flexStartBoxStyle } from "~/styles/boxStyles"
+
+export const homeStyle = {
+    ...flexStartBoxStyle,
+    backgroundColor: "white !important",
+    boxShadow: "0px 4px 6px 2px rgba(0,0,0,0.1)",
+}
