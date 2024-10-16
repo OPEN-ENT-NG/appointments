efc5717f7bcfc703a5eb4d59049c0802258bd69e
AMOSSE Nikolaï <niko@FR-GM7ZTL3.groupinfra.com>
2024-10-16 17:13:05 +0200

    feat(grid): #RDV-23 WIP

--- frontend/src/routes/home/index.tsx
+++ frontend/src/routes/home/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 
 import { ID } from "edifice-ts-client";
-import { Box, Typography } from "@mui/material";
-import { homeStyle, titleStyle } from "./style";
+import { Box, Tab, Tabs, Typography } from "@mui/material";
+import { contentStyle, homeStyle, tabsStyle, titleStyle } from "./style";
 import { useTranslation } from "react-i18next";
 import { MyAvailabilities } from "~/containers/MyAvailabilities";
+import { FindAppointments } from "~/containers/FindAppointments";
+import { MyAppointments } from "~/containers/MyAppointments";
 
 export interface AppProps {
   _id: string;
@@ -20,6 +22,13 @@ export interface AppProps {
 
 export const Home: React.FC = () => {
 
+  const [tabValue, setTabValue] = React.useState(parseInt(sessionStorage.getItem("tabValue")!) | 0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setTabValue(newValue);
+    sessionStorage.setItem("tabValue", newValue.toString());
+  }
+
   const { t } = useTranslation("appointments");
   
   return (
@@ -29,7 +38,17 @@ export const Home: React.FC = () => {
         {t("appointments.title")}
       </Typography>
       </Box>
-      <MyAvailabilities />
+      <Box sx={contentStyle}>
+        <Tabs sx={tabsStyle} value={tabValue} onChange={handleChange}>
+          <Tab label={t("appointments.find.appointment")} />
+          <Tab label={t("appointments.my.appointments")} />
+          <Tab label={t("appointments.my.availability")} />
+        </Tabs>
+        {tabValue === 0 && <FindAppointments />}
+        {tabValue === 1 && <MyAppointments />}
+        {tabValue === 2 && <MyAvailabilities />}
+      </Box>
+      
     </Box>
   );
 };
