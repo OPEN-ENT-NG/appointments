3f5e58b0437d993d4dd6ac6fb448dd2ba38aca99
AMOSSE Nikolaï <niko@FR-GM7ZTL3.groupinfra.com>
2024-10-16 15:45:31 +0200

    feat(grid): #RDV-23 WIP

--- frontend/src/containers/GridModal/index.tsx
+++ frontend/src/containers/GridModal/index.tsx
@@ -1,7 +1,12 @@
 import { Modal } from "@mui/material";
 import { FC } from "react";
+import { useGlobalProvider } from "~/providers/GlobalProvider";
 
 export const GridModal: FC = () => {
+    const { 
+        displayModals : { grid },
+        handleDisplayModal,
+    } = useGlobalProvider();
     return (
         <Modal>
             <div>
