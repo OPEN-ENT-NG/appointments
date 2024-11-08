c95301e312e00d0e5bd86d4c92737a2c3dafd98b
AMOSSE Nikolaï <niko@FR-GM7ZTL3.groupinfra.com>
2024-11-08 14:24:34 +0100

    feat(find): #RDV-27 start find appointment

--- frontend/src/containers/FindAppointments/index.tsx
+++ frontend/src/containers/FindAppointments/index.tsx
@@ -1,5 +1,17 @@
 import { FC } from "react";
 
+import { SearchInput } from "@cgi-learning-hub/ui";
+import { Box } from "@mui/material";
+
+import { containerStyle } from "./style";
+
 export const FindAppointments: FC = () => {
-  return <>TODO</>;
+  return (
+    <Box sx={containerStyle}>
+      <SearchInput />
+      <Box>
+        
+      </Box>
+    </Box>
+  );
 };
---
+++ frontend/src/containers/FindAppointments/style.ts
@@ -0,0 +1,7 @@
+import { columnBoxStyle } from "~/styles/boxStyles";
+
+export const containerStyle = {
+    ...columnBoxStyle,
+    padding: "3rem 3rem",
+    gap: "2rem",
+  };
