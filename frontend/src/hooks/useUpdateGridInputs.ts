bc9cf3cadd17edaa70613f18ba60140c485b27d4
AMOSSE Nikolaï <niko@FR-GM7ZTL3.groupinfra.com>
2024-11-19 19:45:39 +0100

    fix: #RDV-23 make feedback

--- frontend/src/hooks/useUpdateGridInputs.ts
+++ frontend/src/hooks/useUpdateGridInputs.ts
@@ -13,12 +13,13 @@ import { v4 as uuidv4 } from "uuid";
 
 import { Structure, useUpdateGridInputsType } from "./types";
 import { HexaColor } from "~/components/ColorPicker/types";
+import { isTimeInRange } from "~/components/DailySlot/utils";
 import { DAY, PERIODICITY, SLOT_DURATION } from "~/core/enums";
 import {
   INVALID_SLOT_ERROR,
   SAME_GRID_ALREADY_EXISTS_ERROR,
 } from "~/core/i18nKeys";
-import { TimeObject } from "~/core/types";
+import { Slot, TimeObject } from "~/core/types";
 import { formatTimeToDayjs } from "~/core/utils/date.utils";
 import {
   GridModalInputs,
@@ -181,21 +182,20 @@ export const useUpdateGridInputs: useUpdateGridInputsType = (
 
   const handleSlotChange = (
     day: DAY,
-    slotId: string,
+    slot: Slot,
     value: string,
     type: "begin" | "end",
   ) => {
     const [hour, minute] = value.split(":");
     const time: TimeObject = { hour: parseInt(hour), minute: parseInt(minute) };
+    const updatedSlot = {
+      ...slot,
+      [type]: { hour, minute },
+    };
     updateInputField("weekSlots", {
       ...inputs.weekSlots,
       [day]: inputs.weekSlots[day].map((item) => {
-        if (item.id !== slotId) return item;
-
-        const updatedSlot = {
-          ...item,
-          [type]: { hour, minute },
-        };
+        if (item.id !== slot.id) return item;
 
         if (type === "begin" && updatedSlot.end) {
           const begin = formatTimeToDayjs(time);
@@ -206,6 +206,37 @@ export const useUpdateGridInputs: useUpdateGridInputsType = (
         }
 
         return updatedSlot;
+      }),
+    });
+
+    updateInputField("weekSlots", {
+      ...inputs.weekSlots,
+      [day]: inputs.weekSlots[day].map((item) => {
+        if (item.id === slot.id) return updatedSlot;
+        if (item.id !== slot.id) {
+          if (
+            type === "begin" &&
+            !item.end &&
+            item.begin &&
+            updatedSlot.begin &&
+            updatedSlot.end
+          ) {
+            const beginItem = formatTimeToDayjs(item.begin);
+            const beginSlot = formatTimeToDayjs(updatedSlot.begin);
+            const endSlot = formatTimeToDayjs(updatedSlot.end);
+
+            if (
+              isTimeInRange(beginItem, beginSlot, endSlot) ||
+              beginItem.isSame(beginSlot)
+            ) {
+              return {
+                ...item,
+                begin: null,
+              };
+            }
+          }
+          return item;
+        }
       }),
     });
   };
