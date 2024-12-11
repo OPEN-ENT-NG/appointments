affd7ed17bdc5623691628bef39f67edf32b44b5
AMOSSE Nikolaï <niko@FR-GM7ZTL3.groupinfra.com>
2024-12-11 21:57:55 +0100

    rm console

--- frontend/src/components/CustomMultiAutocomplete/index.tsx
+++ frontend/src/components/CustomMultiAutocomplete/index.tsx
@@ -6,8 +6,8 @@ import { Autocomplete, Checkbox, Chip, TextField } from "@mui/material";
 import { useTranslation } from "react-i18next";
 import { v4 as uuidv4 } from "uuid";
 
-import { chipStyle, TextFieldStyle } from "./style";
 import { useGridModal } from "~/providers/GridModalProvider";
+import { chipStyle, TextFieldStyle } from "./style";
 
 export const CustomMultiAutocomplete: FC = () => {
   const {
@@ -18,8 +18,6 @@ export const CustomMultiAutocomplete: FC = () => {
 
   const selectedPublic = inputs.public;
   const { t } = useTranslation("appointments");
-
-  console.log("publicOptions", publicOptions);
 
   return (
     <Autocomplete
