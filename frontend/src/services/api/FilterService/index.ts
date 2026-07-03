4b0d26df1c200b07e1c8558147c80bf4eac27cee
AMOSSE Nikolaï <niko@FR-GM7ZTL3.groupinfra.com>
2026-07-03 16:38:22 +0200

    fix prettier

--- frontend/src/services/api/FilterService/index.ts
+++ frontend/src/services/api/FilterService/index.ts
@@ -4,13 +4,12 @@ import { APPOINTMENT_FILTER_STATE, TagName } from "~/core/enums";
 
 export const filterApi = emptySplitApi.injectEndpoints({
   endpoints: (builder) => ({
-    getAppointmentFilterStates: builder.query<
-      APPOINTMENT_FILTER_STATE[],
-      void
-    >({
-      query: () => "/filters/appointments/states",
-      providesTags: [TagName.APPOINTMENTS],
-    }),
+    getAppointmentFilterStates: builder.query<APPOINTMENT_FILTER_STATE[], void>(
+      {
+        query: () => "/filters/appointments/states",
+        providesTags: [TagName.APPOINTMENTS],
+      },
+    ),
     getAppointmentFilterGrids: builder.query<NameWithId[], void>({
       query: () => "/filters/appointments/grids",
       providesTags: [TagName.APPOINTMENTS],
